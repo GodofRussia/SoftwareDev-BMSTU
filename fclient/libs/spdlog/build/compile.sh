4b47a66bf86d14522308441ce018437b4df078c7
Ilya <2002marsic@gmail.com>
2023-03-18 19:56:14 +0300

    lab 1-3

--- fclient/libs/spdlog/build/compile.sh
+++ fclient/libs/spdlog/build/compile.sh
@@ -1,10 +1,9 @@
-
 #!/bin/bash
 
-#ABI=armaebi-v7a
+ABI=armeabi-v7a
 #ABI=x86
 #ABI=arm64-v8a
-ABI=x86_64
+#ABI=x86_64
 
 ANDROID_NDK=/home/ilya/Android/Sdk/ndk/25.2.9519653/
 TOOL_CHAIN=${ANDROID_NDK}/build/cmake/android.toolchain.cmake
