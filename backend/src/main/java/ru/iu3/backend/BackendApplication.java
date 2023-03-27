aa64863caabbd02adf3f7350e7d630bf5ce3cf7a
Ilya <2002marsic@gmail.com>
2023-03-27 05:00:52 +0300

    lab5 done

--- backend/src/main/java/ru/iu3/backend/BackendApplication.java
+++ backend/src/main/java/ru/iu3/backend/BackendApplication.java
@@ -2,7 +2,11 @@ package ru.iu3.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "ru.iu3.backend.repositories")
+@EntityScan("ru.iu3.backend.models")
 @SpringBootApplication
 public class BackendApplication {
 
